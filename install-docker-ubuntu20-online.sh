3be3183ffa0abb0936dcfcdde33466e4f43e86c6
timmy <593817844@qq.com>
2024-11-15 14:14:43 +0000

    install docker

--- install-docker-ubuntu20-online.sh
+++ install-docker-ubuntu20-online.sh
@@ -1,4 +1,14 @@
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /usr/share/keyrings/docker-archive-keyring.gpg
-add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# 重新导入 GPG 密钥
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# 更新源列表以指定签名密钥
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# 更新软件包列表
 apt update
-apt install docker-ce -y
+# 安装
+apt install docker-ce docker-ce-cli containerd.io -y 
+
+
+# docker-ce：Docker 引擎本身，负责容器的生命周期管理。
+# docker-ce-cli：命令行工具，允许用户与 Docker 引擎交互。
+# containerd：底层容器运行时，负责容器的实际运行和管理。
+
