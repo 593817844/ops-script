b3394b3ae97e4f967be6f9208bee92a5fc102375
Timmy <107969599+593817844@users.noreply.github.com>
2025-06-26 17:37:23 +0800

    Create deploy.sh

---
+++ prometheus/k8s/deploy.sh
@@ -0,0 +1,14 @@
+# 下载分支
+git clone https://github.com/prometheus-operator/kube-prometheus.git --branch release-0.15
+
+cd kube-prometheus
+#首先创建需要的命名空间和 CRD
+kubectl create -f manifests/setup
+
+#可选，修改镜像地址，国内可能无法拉去
+sed -i s/registry.k8s.io/k8s.mirror.nju.edu.cn/ manifests/prometheusAdapter-deployment.yaml
+sed -i s/registry.k8s.io/k8s.mirror.nju.edu.cn/ manifests/kubeStateMetrics-deployment.yaml
+
+
+#创建组件
+kubectl apply -f manifests/
