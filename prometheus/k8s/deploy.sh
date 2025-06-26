a804b40916bdd3033224a31f5313a8e05f5a8036
Timmy <107969599+593817844@users.noreply.github.com>
2025-06-26 17:48:05 +0800

    Update deploy.sh

--- prometheus/k8s/deploy.sh
+++ prometheus/k8s/deploy.sh
@@ -8,7 +8,7 @@ kubectl create -f manifests/setup
 #可选，修改镜像地址，国内可能无法拉去
 sed -i s/registry.k8s.io/k8s.mirror.nju.edu.cn/ manifests/prometheusAdapter-deployment.yaml
 sed -i s/registry.k8s.io/k8s.mirror.nju.edu.cn/ manifests/kubeStateMetrics-deployment.yaml
-
+sed -i s#grafana/grafana:12.0.1#swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/grafana/grafana:12.0.1#  manifests/grafana-deployment.yaml
 
 #创建组件
 kubectl apply -f manifests/
